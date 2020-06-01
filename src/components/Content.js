9d9eb1ce1b1a6c9917409cdf6a2d5c671c2e09d1
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-01 09:20:09 +0300

    rename css file and add .image property in this

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import css from './Content.modele.css'
+import css from './Content.module.css'
 
 
 function Content() {
     return(
         <div className={css.content}>
-            <img className={css.image} src='https://www.onlygfx.com/wp-content/uploads/2017/12/origami-banner-rectangle-2-1024x295.png' width="100%" height="170px"/>
+            <img className={css.image} src='https://www.onlygfx.com/wp-content/uploads/2017/12/origami-banner-rectangle-2-1024x295.png'/>
 
             <div>ava + description</div>
             <div>text area</div>
