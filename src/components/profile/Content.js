e2fa1cdb27698d823d058915b619b0691dd77efa
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-02 08:51:02 +0300

    edit components position

--- src/components/profile/Content.js
+++ src/components/profile/Content.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import css from './Content.module.css'
-
+import Avatar from "./profileAvatar/Ava";
+import Description from "./profileDescription/Description";
 
 function Content() {
     return(
         <div className={css.content}>
             <img className={css.image} src='https://www.onlygfx.com/wp-content/uploads/2017/12/origami-banner-rectangle-2-1024x295.png'/>
-
+            <Avatar />
+            {/*<Description />*/}
             <div>text area</div>
             <div>New posts</div>
             <div>Post 1</div>
