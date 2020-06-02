b2d93500919a08bc62ac52e4241d9850f6bb800b
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-02 08:49:19 +0300

    edit avatar display

--- src/components/profile/profileAvatar/Ava.js
+++ src/components/profile/profileAvatar/Ava.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import css from "./Ava.module.css"
+
 
 function Avatar() {
     return(
-        <div className="Avatar">
-            <img src=""/>
+        <div className={css.avatar}>
+            <img src="https://arrowartwork.in/wp-content/uploads/2019/10/avatar-15.png" width={80} height={80}/>
         </div>
     );
 }
