0aeb29320c281c669289d693c3817685e26d9b16
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-02 08:50:00 +0300

    edit header image :)

--- src/components/alwaysDisplayed/Header.js
+++ src/components/alwaysDisplayed/Header.js
@@ -4,7 +4,7 @@ import css from './Header.module.css'
 function Header() {
     return(
         <header className={css.header}>
-            <img src='./../public/logo192.png'/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png' width={110} height={80}/>
             Header
         </header>
     );
