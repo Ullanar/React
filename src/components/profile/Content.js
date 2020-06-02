4592ae51e27706d2073122fb70c2684e82d1a824
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-02 09:23:28 +0300

    add textarea and button for new posts

--- src/components/profile/Content.js
+++ src/components/profile/Content.js
@@ -2,15 +2,17 @@ import React from 'react';
 import css from './Content.module.css'
 import Avatar from "./profileAvatar/Ava";
 import Description from "./profileDescription/Description";
+import CreateNewPost from  './createNewPost/Textarea'
+
 
 function Content() {
     return(
         <div className={css.content}>
             <img className={css.image} src='https://www.onlygfx.com/wp-content/uploads/2017/12/origami-banner-rectangle-2-1024x295.png'/>
-            <Avatar /> <Description />
-
-            <div>text area</div>
-            <div>New posts</div>
+            <Avatar />
+            <Description />
+            <br/>
+            <CreateNewPost />
             <div>Post 1</div>
             <div>Post 2</div>
 
--- src/components/profile/createNewPost/Textarea.js
+++ src/components/profile/createNewPost/Textarea.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Textarea() {
     return(
       <div>
-
+        <textarea>Create new post!</textarea><button>Submit</button>
       </div>
     );
 }
+
+export default Textarea;
