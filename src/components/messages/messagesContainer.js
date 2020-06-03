3c5f8ff3eda7a893fddddfb9c055807cf28afb16
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-03 05:05:17 +0300

    add container for messages selection

---
+++ src/components/messages/messagesContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './messagesContainer.module.css'
+
+function Container() {
+    return(
+      <div className={css.container}>
+
+      </div>
+    );
+}
