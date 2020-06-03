232e86666465de0d386899ee86b3ed5054d9eb01
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-03 11:26:37 +0300

    add new test pages

---
+++ src/components/faq/FAQ.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from "./Faq.module.css";
+
+function Container() {
+    return(
+        <div className={css.container}>
+            Messages
+        </div>
+    );
+}
--- src/components/messages/messagesContainer.js
+++ src/components/messages/messagesContainer.js
@@ -4,7 +4,7 @@ import css from './messagesContainer.module.css'
 function Container() {
     return(
       <div className={css.container}>
-
+        Messages
       </div>
     );
 }
