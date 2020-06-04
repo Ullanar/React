d4d2b815c3374bfdd15e0d88fae45c4d5c1a7cef
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-05 00:06:50 +0300

    add new features for messages display

--- src/components/messages/messagesContainer.js
+++ src/components/messages/messagesContainer.js
@@ -6,35 +6,35 @@ import ChooseUser from "./ChooseUser/ChooseUser";
 
 function MessagesContainer() {
     let usersDataTemplates = [
-        { userName : 'Miku', dialogId : '1', },
-        { userName : 'Vaffe', dialogId : '2', },
-        { userName : 'Aurelian', dialogId : '3', },
-        { userName : 'Attache', dialogId : '4', },
-        { userName : 'Alex', dialogId : '5', }
-        ];
+        {userName: 'Miku', dialogId: '1',},
+        {userName: 'Vaffe', dialogId: '2',},
+        {userName: 'Aurelian', dialogId: '3',},
+        {userName: 'Attache', dialogId: '4',},
+        {userName: 'Alex', dialogId: '5',}
+    ];
 
     let messagesDataTemplates = [
-        { messageText : 'Hi', messageId : '1', },
-        { messageText : 'glhf', messageId : '2', },
-        { messageText : 'gg wp', messageId : '3', },
-        { messageText : 'Mhhhh... nice team', messageId : '4', },
-        { messageText : 'BACKDOOOOR!!!', messageId : '5', }
+        {messageText: 'Hi', messageId: '1',},
+        {messageText: 'glhf', messageId: '2',},
+        {messageText: 'gg wp', messageId: '3',},
+        {messageText: 'Mhhhh... nice team', messageId: '4',},
+        {messageText: 'BACKDOOOOR!!!', messageId: '5',}
     ];
 
 
     let userDate = usersDataTemplates
-        .map( (dialog) => <ChooseUser userName= {dialog.userName} dialogId={dialog.dialogId}/> );
+        .map((dialog) => <ChooseUser userName={dialog.userName} dialogId={dialog.dialogId}/>);
 
 
     let messagesData = messagesDataTemplates
-        .map( (message) => <Message messageText = {message.messageText}/>);
+        .map((message) => <Message messageText={message.messageText}/>);
 
     return (
         <div className={css.container}>
             <div className={css.dialogs}>
                 <ul>
                     {userDate}
-                {/*    Тут мы зафигачили массив userDate в разметку JSX*/}
+                    {/*    Тут мы зафигачили массив userDate в разметку JSX*/}
                 </ul>
             </div>
 
