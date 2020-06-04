e8ddff3898fb8dbb2ede1b288e32e4fe4ca93e65
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-05 00:04:51 +0300

    add new features for messages display

--- src/components/messages/messagesContainer.js
+++ src/components/messages/messagesContainer.js
@@ -5,7 +5,7 @@ import ChooseUser from "./ChooseUser/ChooseUser";
 
 
 function MessagesContainer() {
-    let usersData = [
+    let usersDataTemplates = [
         { userName : 'Miku', dialogId : '1', },
         { userName : 'Vaffe', dialogId : '2', },
         { userName : 'Aurelian', dialogId : '3', },
@@ -13,7 +13,7 @@ function MessagesContainer() {
         { userName : 'Alex', dialogId : '5', }
         ];
 
-    let messagesData = [
+    let messagesDataTemplates = [
         { messageText : 'Hi', messageId : '1', },
         { messageText : 'glhf', messageId : '2', },
         { messageText : 'gg wp', messageId : '3', },
@@ -21,25 +21,26 @@ function MessagesContainer() {
         { messageText : 'BACKDOOOOR!!!', messageId : '5', }
     ];
 
+
+    let userDate = usersDataTemplates
+        .map( (dialog) => <ChooseUser userName= {dialog.userName} dialogId={dialog.dialogId}/> );
+
+
+    let messagesData = messagesDataTemplates
+        .map( (message) => <Message messageText = {message.messageText}/>);
+
     return (
         <div className={css.container}>
             <div className={css.dialogs}>
                 <ul>
-
-                    <ChooseUser userName= {usersData[0].userName} dialogId={usersData[0].dialogId}/>
-                    <ChooseUser userName= {usersData[1].userName} dialogId={usersData[1].dialogId}/>
-                    <ChooseUser userName= {usersData[2].userName} dialogId={usersData[2].dialogId}/>
-                    <ChooseUser userName= {usersData[3].userName} dialogId={usersData[3].dialogId}/>
-                    <ChooseUser userName= {usersData[4].userName} dialogId={usersData[4].dialogId}/>
+                    {userDate}
+                {/*    Тут мы зафигачили массив userDate в разметку JSX*/}
                 </ul>
             </div>
 
             <div className={css.messages}>
-                <Message messageText = {messagesData[0].messageText}/>
-                <Message messageText = {messagesData[1].messageText}/>
-                <Message messageText = {messagesData[2].messageText}/>
-                <Message messageText = {messagesData[3].messageText}/>
-                <Message messageText = {messagesData[4].messageText}/>
+                {messagesData}
+                {/*    Тут мы зафигачили массив messagesDate в разметку JSX*/}
             </div>
         </div>
     );
