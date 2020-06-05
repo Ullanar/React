82abc6aec22a8e68914a8c63a3ba3e60698a8c75
Ullanar <65561785+Ullanar@users.noreply.github.com>
2020-06-06 01:22:04 +0300

    add Redux in project

--- src/components/profile/createNewPost/Textarea.js
+++ src/components/profile/createNewPost/Textarea.js
@@ -2,6 +2,8 @@ import React from 'react';
 import css from './Textarea.module.css'
 
 function Textarea() {
+
+
     return (
         <div className={css.textarea}>
             <textarea>Create new post!</textarea>
