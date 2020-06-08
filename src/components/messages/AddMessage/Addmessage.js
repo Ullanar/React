320adfdc63c0eb40d4eb14a6f58d58d8dd7eb01e
Bubnov.V.Yur <Bubnov.V.Yur@sberbank.ru>
2020-06-08 18:46:34 +0300

    code refactoring

--- src/components/messages/AddMessage/Addmessage.js
+++ src/components/messages/AddMessage/Addmessage.js
@@ -4,20 +4,21 @@ import store from "../../../redux/state";
 
 function AddMessage(props) {
 
-    function newMessage () {
-        props.dispatch({type : 'ADD-MESSAGE'});
+    function newMessage() {
+        props.dispatch({type: 'ADD-MESSAGE'});
         newMessageElement.current.value = '';
 
     }
-    function onMessageChange () {
+
+    function onMessageChange() {
         let newTextDisplay = newMessageElement.current.value;
-        store.dispatch({type : 'UPDATE-NEW-MESSAGE-TEXT', newTextDisplay: newTextDisplay});
+        store.dispatch({type: 'UPDATE-NEW-MESSAGE-TEXT', newTextDisplay: newTextDisplay});
     }
 
     let newMessageElement = React.createRef();
     return (
         <div className={css.textarea}>
-            <textarea ref = {newMessageElement} onChange={onMessageChange} value={props.newDialogMessageDisplay}/>
+            <textarea ref={newMessageElement} onChange={onMessageChange} value={props.newDialogMessageDisplay}/>
             <button onClick={newMessage}>Add message</button>
         </div>
     );
